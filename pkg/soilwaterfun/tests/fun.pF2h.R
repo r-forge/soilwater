abb24f23cccadfab44db41061540326bb779bc1f
jmoeys <jmoeys@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-23 13:50:59 +0000

    (start from zero again to solve the mess)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/soilwater@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/soilwaterfun/tests/fun.pF2h.R
+++
@@ -1,3 +0,0 @@
-require( "soilwaterfun" )
-
-fun.pF2h( pF = c(0,2,4.2) ) 
--- pkg/soilwaterfun/tests/fun.vangenuchten.se.h.R
+++
@@ -1,9 +0,0 @@
-require( "soilwaterfun" ) 
-
-# Example with the properties of the Footprint soil type P22i, 
-# 3rd layer:
-fun.vangenuchten.se.h( 
-    h       = -c(0,0.01,0.1,1,10,100,158), 
-    alpha   = 3.561099, 
-    n       = 1.212074  
-)   #
