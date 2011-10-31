fd77767dc67aac2b7e4e6a6d3cdaca453d92c751
jmoeys <jmoeys@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-31 16:33:06 +0000

    minor change
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/soilwater@31 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/soilwaterptf/R/onAttach.R
+++ pkg/soilwaterptf/R/onAttach.R
@@ -2,8 +2,8 @@
  libname, 
  pkgname  
 ){  #
-    cat("'soilwaterptf' has been loaded.\n") 
-    cat("type help(package='soilwaterptf') to get started") 
+    cat("'", pkgname, "' package loaded.\n") 
+    cat("type help( package='", pkgname, "') to get started\n") 
     # cat("Please notice that a bug has been found in Wosten kSat calculations:\n")
     # cat("\tkSat was reported to be calculated in [mm.h-1], while it was [cm.day-1].\n")
     # cat("\tSorry for the inconvenience.\n")
