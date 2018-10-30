4ec646aab319094701cffb2aaf7cd61b76e2fe42
Julien Moeys <julienmoeys@yahoo.fr>
2018-10-30 10:06:00 +0100

    Remove old infrastructure to build and check package

--- soilwaterptf_1_RCMDBUILD.bat
+++
@@ -1,9 +0,0 @@
-set pkgname=soilwaterptf
-
-cd /D "%rPackagesDir%\soilwater\pkg" 
-
-svnversion > %pkgname%\inst\SVN_VERSION
-
-R CMD build --compact-vignettes="gs" --md5 %pkgname% 
-
-pause
--- soilwaterptf_fortran_compile2.bat
+++
@@ -1,8 +0,0 @@
-c:
-cd \
-cd "_SOILWATER\pkg\soilwaterptf\src"
-pause 
-
-R CMD SHLIB -O3  -c  soilwaterptf.f90 -o soilwaterptf.o
-
-pause
